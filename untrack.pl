c993d9d5a3065198cb898fe698903710067ae481
xzvf <xzvf@su-root.cc>
2020-08-04 10:20:53 +0200

    script done

---
+++ untrack.pl
@@ -0,0 +1,37 @@
+#!/bin/env perl
+
+use strict;
+use warnings;
+use URI;
+
+# change to script path for the filter file
+use FindBin qw($RealBin);
+chdir $RealBin;
+
+sub help() {
+    print "untrack.pl - removes trackers from URLs
+USAGE:
+    untrack.pl <URL>
+(c) xzvf
+";
+exit 1;
+}
+
+my ($urlstr) = @ARGV;
+defined $urlstr or help;
+my $uri = URI->new($urlstr);
+my $query = $uri->query;
+$query or print $uri->as_string and exit;
+my @qa = split /&/, $query;
+# filter
+open my $filter, '<', 'filterrules' or die "failed to open filterrules file";
+while(my $rule = <$filter>) {
+    chomp $rule;
+    @qa = grep ! /$rule/, @qa;
+}
+
+# reconstruct URL
+my $newquery = join '&', @qa;
+$uri->query($newquery);
+print $uri->as_string;
+
